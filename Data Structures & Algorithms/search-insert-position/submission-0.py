7900e5f6e6268696652e8e4bd1741c32ec6717e5
aloftus9 <76117875+aloftus9@users.noreply.github.com>
2026-06-04 08:39:08 -0400

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.py
@@ -0,0 +1,22 @@
+
+# Classic binary search but return index of where it should go if not
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+
+            mid = (left + right) // 2
+
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+        
+        return left
+        
