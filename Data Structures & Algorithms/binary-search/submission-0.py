2d8436970914074a73863452c10a5e3b45ea08cb
aloftus9 <76117875+aloftus9@users.noreply.github.com>
2026-06-04 08:32:09 -0400

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -0,0 +1,26 @@
+
+# Keep looking until the middle = target
+# [-1,0,2,4,6,8]
+# 0, 5
+# 2.5 
+# 
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        left = 0
+        right = len(nums) - 1
+
+
+        while left <= right:
+            mid = (left+right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
+
+        
