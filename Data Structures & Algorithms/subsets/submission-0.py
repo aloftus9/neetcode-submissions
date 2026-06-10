fce5bb86bc9e2786420f9e3373cb65299c725676
aloftus9 <76117875+aloftus9@users.noreply.github.com>
2026-06-10 09:14:19 -0400

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,25 @@
+
+# Basically going to dfs down every choice of adding/removinga number
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+
+        result = []
+        subset = []
+
+        def backtrack(index):
+
+            if index == len(nums):
+                result.append(subset.copy())
+                return
+
+
+            subset.append(nums[index])
+            backtrack(index + 1)
+
+            subset.pop()
+            backtrack(index + 1)
+
+        backtrack(0)
+        return result
